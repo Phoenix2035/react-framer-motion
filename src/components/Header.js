408cc18f76a5b28ea8ecae8252a9ef7bee811a67
Ali Gh <aligodosi@gmail.com>
2021-10-12 13:28:31 +0330

    add some animation to Base

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
 import { motion } from "framer-motion";
 
-
-
 const Header = () => {
     return (
         <header>
             <div className="logo">
-                <svg className="pizza-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+                <svg
+                    className="pizza-svg"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 100 100"
+                >
                     <path
                         fill="none"
                         d="M40 40 L80 40 C80 40 80 80 40 80 C40 80 0 80 0 40 C0 40 0 0 40 0Z"
@@ -17,15 +19,16 @@ const Header = () => {
                     />
                 </svg>
             </div>
-            <motion.div className="title"
+            <motion.div
+                className="title"
                 initial={{ y: -250 }}
                 animate={{ y: -10 }}
-                
+                transition={{ delay: 0.2, type: "spring", stiffness: 120 }}
             >
                 <h1>Pizza Joint</h1>
             </motion.div>
         </header>
-    )
-}
+    );
+};
 
 export default Header;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,21 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
-
 
 const Home = () => {
     return (
-        <motion.div className="home container"
+        <motion.div
+            className="home container"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
+            transition={{ delay: 0.5, duration: 0.5 }}
         >
-            <h2>
-                Welcome to Pizza Joint
-            </h2>
+            <h2>Welcome to Pizza Joint</h2>
 
             <Link to="/base">
-                <motion.button animate={{}}>
-                    Create Your Pizza
-                </motion.button>
+                <motion.button animate={{}}>Create Your Pizza</motion.button>
             </Link>
         </motion.div>
-    )
-}
+    );
+};
 
 export default Home;
