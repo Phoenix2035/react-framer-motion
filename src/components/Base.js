44405f1beda279a878d431950db66db7a2819a9f
Ali Gh <aligodosi@gmail.com>
2021-10-12 17:18:27 +0330

    add while hover

--- src/components/Base.js
+++ src/components/Base.js
@@ -1,5 +1,5 @@
-import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const Base = ({ addBase, pizza }) => {
     const bases = ["Classic", "Thin & Crispy", "Thick Crust"];
@@ -16,9 +16,18 @@ const Base = ({ addBase, pizza }) => {
                 {bases.map((base) => {
                     let spanClass = pizza.base === base ? "active" : "";
                     return (
-                        <li key={base} onClick={() => addBase(base)}>
+                        <motion.li
+                            key={base}
+                            onClick={() => addBase(base)}
+                            whileHover={{
+                                scale: 1.3,
+                                color: "#f8e112",
+                                originX: 0,
+                            }}
+                            transition={{ type: "spring", stiffness: 300 }}
+                        >
                             <span className={spanClass}>{base}</span>
-                        </li>
+                        </motion.li>
                     );
                 })}
             </ul>
@@ -31,7 +40,15 @@ const Base = ({ addBase, pizza }) => {
                     transition={{ type: "spring", stiffness: 120 }}
                 >
                     <Link to="/toppings">
-                        <button>Next</button>
+                        <motion.button
+                            whileHover={{
+                                scale: 1.1,
+                                textShadow: "0 0 8px rgb(255,255,255)",
+                                boxShadow: "0 0 8px rgb(255,255,255)",
+                            }}
+                        >
+                            Next
+                        </motion.button>
                     </Link>
                 </motion.div>
             )}
